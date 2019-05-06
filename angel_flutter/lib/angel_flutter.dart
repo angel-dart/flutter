13a45699a44b708c2816af5009785b4c629a5ebf
Tobe O <thosakwe@gmail.com>
2019-05-05 20:22:55 -0400

    V2

--- angel_flutter/lib/angel_flutter.dart
+++ angel_flutter/lib/angel_flutter.dart
@@ -1,2 +1,3 @@
 export 'package:angel_client/flutter.dart';
-export 'ui/angel_animated_list.dart';
+export 'src/ui/angel_animated_list.dart';
+export 'src/ui/request_builder.dart';
--- example_common/lib/example_common.dart
+++
@@ -1,1 +0,0 @@
-export 'src/models/todo.dart';
